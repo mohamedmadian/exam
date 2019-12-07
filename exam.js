84f3edb64b3bd5212c76de1dc50453369eaeac1c
mohamedmadian <mohamed.madean1996@gmail.com>
2019-12-07 10:08:22 +0200

    commit exam.html

---
+++ exam.js
@@ -0,0 +1,44 @@
+var examObj={
+    name:"C Exam", 
+    time: 30,
+    questions:[
+                { 
+                    head: "Full form of URL is ?" ,
+                    answers :[
+                        "a.Uniform Resource Locator",
+                        "b.Uniform Resource Link",
+                        "c.Uniform Registered Locator",
+                        "d.Unified Resource Link"
+                
+                    ],
+                    correctAnswer:0,
+                    grade:1
+
+                },
+                { 
+                    head: "in which of the following form, data is stored in computer ?" ,
+                    answers :[
+                        "a.Decimal",
+                        "b.Binary",
+                        "c.HexaDecimal",
+                        "d.Octal"
+                
+                    ],
+                    correctAnswer:1,
+                    grade:1
+
+                }
+            ]
+};
+
+window.addEventListener('load',function(){
+    //select window div 
+    //dispaly one obj 
+    document.getElementById("window")
+
+
+
+
+});   //end of load
+
+
